af9f5a35e908eae02426abdabc9c610d626735bb
Sam Scheffler <schefflersam@gmail.com>
2015-09-20 20:37:40 -0500

    Added fullscreen functionality
    -Added fullscreen functionality
    -Added more text to meditation

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 $(function() {
     var _startButton, _pulsingRing, _helpText, _textArray, 
         _guidingText, _musicOptionButton,
-        _fullscreenButton= null;
+        _fullscreenButton, _fullscreenText= null;
 
     initialize();
     createTextArray();
@@ -13,13 +13,27 @@ function initialize(){
     _fullscreenButton   = $("#fullscreenButton");
     _helpText           = $("#helpText");
     _optionButtons      = $(".optionButton");
+    _fullscreenText     = $("#fullscreenOnOffText");
 
     _helpText.fadeIn(2000);
     _startButton.delay(1500).fadeIn(1200);
     _musicOptionButton.delay(1700).fadeIn(1200);
     _fullscreenButton.delay(1700).fadeIn(1200);
-
-
+  
+    //Fullscreen functionality
+    _fullscreenButton.click(function(){
+      if(_fullscreenText.hasClass("fullscreenOff")){
+        _fullscreenText.text("Fullscreen: On");
+        _fullscreenText.removeClass("fullscreenOff");
+      }
+      else{
+        _fullscreenText.text("Fullscreen: Off");
+        _fullscreenText.addClass("fullscreenOff");
+      }
+      
+      toggleFullScreen();
+    });
+  
     _startButton.click(function(){
         $("div").removeClass("hoverActive");
         _helpText.fadeOut(500);
@@ -89,13 +103,43 @@ function changeText(array, counter) {
     }
 }
 
+//Text for the meditation
 function createTextArray() {
     _textArray = [
         "Relax", "Take the next few minutes for yourself", "Focus on the dot", "Let your breathing follow it", "Breathing in as it expands",
-        "And breathing out as it contracts", "And as you breathe in and out", "Allow every muscle in your body", "To relax"
+        "And breathing out as it contracts", "And as you breathe in and out", "Allow every muscle in your body", "To relax", "And let every bit of stress",
+        "Start to fade away", "With every breath out", "Exhale any worries", "Any tension", "Any stress of the day", "Allow yourself this time",
+        "To relax", " ", "And as you breathe", "Imagine that there is a warm, relaxing wave", "Starting to form at your toes", "And with each deep breath", 
+        "This wave moves upwards slightly"
     ];
 }
 
 function endMeditation() {
 
+}
+
+//Code from https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
 }
